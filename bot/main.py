5777df0cc75557a1854ab6a8bf04ccce317681d6
mishail <mishayugai53@gmail.com>
2021-11-20 21:11:17 +0300

    first enter

---
+++ bot/main.py
@@ -0,0 +1,22 @@
+import telebot
+from telebot import types
+
+token = "2113711681:AAFI6-N9E3ucGERZVZy8o2l2D94TGQjZPDA"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу", "/help")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею...')
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда тебе сюда – https://mtuci.ru/')
+
+bot.infinity_polling()
